70dbc335123ede5d63c6ebbbb0a360572611089f
Brian Sierakowski <briansierakowski@gmail.com>
2011-06-28 09:38:49 -0400

    Added logic to test for existance of DB in script, creates it if needed. Checks number of entries in the DB vs number of questions on SO, if they're equal, do nothing, if no, adds ALL questions to db (no smart check), if number in DB are higher than questions on SO, throws error

--- sotest.py
+++ sotest.py
@@ -1,11 +1,24 @@
 import stackexchange 
 import sqlite3
+import os.path
+import create_db
 
 site = stackexchange.Site("api.stackoverflow.com")
 
 user = site.user(637160)
 allquestions = user.questions.fetch()
 
+#DB Check
+if os.path.isfile("soquestions.db") == False:
+    print "Creating soquestions database."
+    con = sqlite3.connect('soquestions.db') # Warning: This file is created in the current directory
+    con.execute("CREATE TABLE so (id INTEGER PRIMARY KEY, questionid INTEGER, numberofanswers INTEGER)")
+    con.commit()
+else:
+    print "Database exists, checking number of questions."
+
+
+    
 numberofquestions = len(allquestions)
 print "number of questions for %s on SO: %d" % (user.display_name, numberofquestions)
 
@@ -21,21 +34,23 @@ print "number of questions for user in DB: %d" % (dbquestioncount)
 
 count = 0 
 
+if numberofquestions == dbquestioncount:
+    print "No Changes!"
+elif dbquestioncount > allquestions:
+    print "There's a problem!"
+else:
+    for entry in allquestions:
+        questionid = entry.id
+        answers = site.question(questionid).answers
+        numberofanswers = len(answers)
+        conn = sqlite3.connect('soquestions.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO so (questionid,numberofanswers) VALUES (?,?)", (questionid,numberofanswers))
+        conn.commit()
+        conn.close()
+        print "Current count: %d" % count
+        print "Current Question ID: %d" % questionid
+        print "Number of answers: %d" % numberofanswers
+        print " "
+        count += 1     
 
-raw_input('> ')
-
-for entry in allquestions:
-    questionid = entry.id
-    answers = site.question(questionid).answers
-    numberofanswers = len(answers)
-    conn = sqlite3.connect('soquestions.db')
-    c = conn.cursor()
-    c.execute("INSERT INTO so (questionid,numberofanswers) VALUES (?,?)", (questionid,numberofanswers))
-    conn.commit()
-    conn.close()
-    print "Current count: %d" % count
-    print "Current Question ID: %d" % questionid
-    print "Number of answers: %d" % numberofanswers
-    print " "
-     
-    count += 1 
