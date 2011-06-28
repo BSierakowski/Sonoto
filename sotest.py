fc6a45c4c73c8b7a820286fca27f5a089dd02090
Brian Sierakowski <briansierakowski@gmail.com>
2011-06-28 09:18:04 -0400

    Added logic at the beginning to test # of Question on SO vs number of question in the local DB, will make it skip adding questions if numbers are equal

--- sotest.py
+++ sotest.py
@@ -6,8 +6,23 @@ site = stackexchange.Site("api.stackoverflow.com")
 user = site.user(637160)
 allquestions = user.questions.fetch()
 
+numberofquestions = len(allquestions)
+print "number of questions for %s on SO: %d" % (user.display_name, numberofquestions)
+
+dbquestioncount = 0
+
+conn = sqlite3.connect('soquestions.db')
+c = conn.cursor()
+c.execute('select * from so')
+for rows in c:
+    dbquestioncount += 1
+    
+print "number of questions for user in DB: %d" % (dbquestioncount)
 
 count = 0 
+
+
+raw_input('> ')
 
 for entry in allquestions:
     questionid = entry.id
